eaea689df0324836005f50f8c3a85a17f8999200
Tskading <Tskading@gmail.com>
2021-01-08 14:13:36 -0800

    Initial file creation and basic setup.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
